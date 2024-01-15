36f16f9f22404146ca1b1f726bbb929d522bc4e2
SurhanZahid <surhanzahid@gmail.com>
2024-01-16 00:46:28 +0300

    added checkbox script

---
+++ scripts/checkbox.js
@@ -0,0 +1,69 @@
+const uniqueCategories = [];
+let filterTest = [];
+
+const renderCheckbox = () => {
+    var items = document.querySelectorAll('.w-dyn-item');
+
+    items.forEach(function (item) {
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+
+        var anchorLink = item.querySelector('.product-tab-link');
+        var anchorText = anchorLink ? anchorLink.textContent : '';
+
+        checkbox.id = anchorText.toLowerCase().replace(/\s/g, '-') + '-checkbox';
+
+        item.insertBefore(checkbox, item.firstChild);
+
+        item.classList.add('product-item-container');
+    });
+}
+
+const filterByCategory = () => {
+    uniqueCategories.forEach(category => {
+        data.forEach(product => {
+            if (product['Product Category'] === category.toLowerCase()) {
+                filterTest.push(product)
+            }
+        })
+    })
+    filteredData = filterTest;
+    refreshList();
+}
+
+const checkboxEventHandler = () => {
+    var anchorLinks = document.querySelectorAll('.product-tab-link');
+    // Loop through each anchor tag
+    anchorLinks.forEach(function (anchor) {
+        // Replace the href attribute with #
+        anchor.href = '#';
+
+        // Add a click event listener to check and uncheck the checkbox
+        anchor.addEventListener('click', function () {
+            // Get the parent element (w-dyn-item)
+            var listItem = anchor.parentElement;
+
+            // Get the checkbox within the parent element
+            var checkbox = listItem.querySelector('input[type="checkbox"]');
+
+            // Toggle the checkbox state
+            checkbox.checked = !checkbox.checked;
+            if (checkbox.checked) {
+                uniqueCategories.push(anchor.textContent);
+                filterByCategory();
+            } else {
+                const index = uniqueCategories.findIndex(category => category === anchor.textContent);
+                uniqueCategories.splice(index, 1);
+                filterTest = []
+                if (uniqueCategories.length) {
+                    filterByCategory();
+                } else {
+                    resetFilter();
+                }
+            }
+        });
+    });
+}
+
+renderCheckbox();
+checkboxEventHandler();
