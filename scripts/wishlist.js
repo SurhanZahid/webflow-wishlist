a862602e1cc7c12aa59094aa3fb50a41e20d11e2
SurhanZahid <surhanzahid@gmail.com>
2024-01-17 23:48:41 +0300

    added whishlist script

---
+++ scripts/wishlist.js
@@ -0,0 +1,140 @@
+const deleteItemFromLocalStorage = (image, text, container) => {
+    const savedData = getSavedData();
+
+    // Find the index of the item to be deleted
+    const indexToDelete = savedData.findIndex(item => item.image === image && item.text === text);
+
+    if (indexToDelete !== -1) {
+        // Remove the item from the array
+        savedData.splice(indexToDelete, 1);
+
+        // Save the updated array back to local storage
+        localStorage.setItem('lookbook', JSON.stringify(savedData));
+        container.classList.toggle('not-selected');
+        handleCounter();
+        console.log('Item deleted:', { image, text });
+
+    } else {
+        console.log('Item not found:', { image, text });
+    }
+}
+const saveToLocalStorage = (image, text, container) => {
+    // Retrieve existing data from local storage or initialize an empty array
+    const lookbookItems = localStorage.getItem('lookbook');
+    const existingData = lookbookItems ? JSON.parse(lookbookItems) : [];
+    const bookmarkDiv = container.querySelector('.bookmark');
+    const bookmarkCheckedDiv = container.querySelector('.bookmark-selected');
+
+    // Check for duplicates based on image and text
+    const isDuplicate = existingData.some(item => item.image === image && item.text === text);
+    // If it's not a duplicate, add the new data
+    if (!isDuplicate) {
+        //container.classList.toggle('selected')
+        bookmarkDiv.classList.toggle('bookmark-selected')
+        const priceContainer = container.querySelectorAll('.product-price');
+        const price = priceContainer[2].innerText
+        existingData.push({ image, text, price });
+        handleCounter();
+        // Save the updated array back to local storage
+        localStorage.setItem('lookbook', JSON.stringify(existingData));
+    } else {
+        //container.classList.toggle('selected');
+        bookmarkCheckedDiv.className = 'bookmark'
+        deleteItemFromLocalStorage(image, text, container)
+        handleCounter();
+        console.log('This image and text combination is already in the array.');
+    }
+}
+const handleOnClick = () => {
+    document.addEventListener('click', function (event) {
+        const clickedElement = event.target.closest('.product-container');
+
+        if (clickedElement) {
+            const image = clickedElement.querySelector('img');
+            const textDiv = clickedElement.querySelector('.product-name');
+
+            const src = image.getAttribute('src');
+            const text = textDiv.textContent;
+            saveToLocalStorage(src, text, clickedElement);
+            handleCounter();
+        }
+    });
+}
+
+const updateItemBorders = () => {
+    const itemsContainer = document.getElementsByClassName('product-cms-item w-dyn-item');
+    const savedData = getSavedData();
+    for (let parent of itemsContainer) {
+        const items = parent.querySelectorAll('.product-container');
+        items.forEach(item => {
+            const image = item.querySelector('img');
+            const textDiv = item.querySelector('.product-name');
+            const src = image.getAttribute('src');
+            const text = textDiv.textContent;
+            const bookmark = item.querySelectorAll('.product-description-wrapper');
+            const bookmarkDiv = document.createElement('div');
+            bookmarkDiv.className = 'bookmark';
+            bookmark[0].appendChild(bookmarkDiv);
+            if (isItemInLocalStorage(src, text)) {
+                bookmarkDiv.className = 'bookmark-selected'
+            }
+        });
+    }
+}
+
+const isItemInLocalStorage = (image, text) => {
+    const savedData = getSavedData();
+    return savedData.some(item => item.image === image && item.text === text);
+}
+
+const getSavedData = () => {
+    const lookbookItems = localStorage.getItem('lookbook');
+    const savedData = lookbookItems ? JSON.parse(lookbookItems) : [];
+    return savedData;
+}
+
+const createCounter = () => {
+    const projectTopContainer = document.querySelectorAll('.lab-nav-start')
+    const projectMobileTopContainer = document.querySelectorAll('.nav-menu-right')
+    const lookbookItems = localStorage.getItem('lookbook');
+    const totalItemsSaved = lookbookItems ? JSON.parse(lookbookItems) : [];
+
+    const counterDiv = document.createElement('span')
+    counterDiv.className = 'counter';
+    const counter2Div = document.createElement('span')
+    counter2Div.className = 'counter2';
+    const counterSpanMobile = document.createElement('span')
+    counterSpanMobile.className = 'counter-mobile';
+    if (totalItemsSaved) {
+        counterDiv.textContent = ' ' + totalItemsSaved.length
+        counter2Div.textContent = ' ' + totalItemsSaved.length
+        counterSpanMobile.textContent = ' ' + totalItemsSaved.length
+    } else {
+        counterDiv.textContent = ' 0'
+        counter2Div.textContent = ' 0'
+        counterSpanMobile.textContent = ' 0'
+    }
+    projectTopContainer[0].appendChild(counterDiv);
+    projectTopContainer[1].appendChild(counter2Div);
+    projectMobileTopContainer[1].appendChild(counterSpanMobile);
+}
+
+const handleCounter = () => {
+    const projectTopContainer = document.querySelectorAll('.counter')
+    const project2TopContainer = document.querySelectorAll('.counter2')
+    const lookbookItems = localStorage.getItem('lookbook');
+    const totalItemsSaved = lookbookItems ? JSON.parse(lookbookItems) : [];
+
+    if (totalItemsSaved !== null && totalItemsSaved.length !== 0) {
+        projectTopContainer[0].textContent = " " + totalItemsSaved.length
+        project2TopContainer[0].textContent = " " + totalItemsSaved.length
+    } else {
+        projectTopContainer[0].textContent = " "
+        project2TopContainer[0].textContent = " "
+    }
+}
+
+window.addEventListener('load', () => {
+    handleOnClick();
+    createCounter();
+});
