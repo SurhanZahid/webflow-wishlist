dbe857be6659afd25a1166bd0f46b3b1b0a0ec5d
SurhanZahid <surhanzahid@gmail.com>
2024-01-20 01:45:48 +0300

    updated price range

--- scripts/priceRange.js
+++ scripts/priceRange.js
@@ -1,32 +1,31 @@
 const renderPriceRange = () => {
     setTimeout(() => {
-        console.log(filteredData)
+        $(function () {
+            let idOfRangeSlider = 'slider-range';
+            let max = Math.max(...filteredData.map(o => o.Price))
+            let min = Math.min(...filteredData.map(o => o.Price))
+            $('#' + idOfRangeSlider).css('display', 'none');
+            $("<div></div>").insertAfter('#' + idOfRangeSlider);
+
+            var range = $('#' + idOfRangeSlider).next()[0];
+
+            noUiSlider.create(range, {
+                connect: true,
+                start: [min, max],
+                range: {
+                    'min': min,
+                    'max': max
+                },
+                step: 10,
+                tooltips: [
+                    wNumb({ decimals: 2, prefix: '$' }),
+                    wNumb({ decimals: 2, prefix: '$' }),
+                ],
+            });
+            range.noUiSlider.on('update', function (values) {
+                $('#' + idOfRangeSlider).val(values.join(' - '));
+            });
+        });
     }, 3000)
-    $(function () {
-        //THIS IS FOR THE RANGE SLIDER.
-        //Sorry for yelling...
-        let idOfRangeSlider = 'slider-range';
-        $('#' + idOfRangeSlider).css('display', 'none');
-        $("<div></div>").insertAfter('#' + idOfRangeSlider);
-
-        var range = $('#' + idOfRangeSlider).next()[0];
-
-        noUiSlider.create(range, {
-            connect: true,
-            start: [5, 68],
-            range: {
-                'min': 2,
-                'max': 200
-            },
-            step: 10,
-            tooltips: [
-                wNumb({ decimals: 2, prefix: '$' }),
-                wNumb({ decimals: 2, prefix: '$' }),
-            ],
-        });
-        range.noUiSlider.on('update', function (values) {
-            $('#' + idOfRangeSlider).val(values.join(' - '));
-        });
-    });
 }
 renderPriceRange()
