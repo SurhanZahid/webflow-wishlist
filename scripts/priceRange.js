f7374f982ed82a2002e4b05fe4ffebb6bb56eec1
SurhanZahid <surhanzahid@gmail.com>
2024-01-19 23:15:29 +0300

    uncommented the render price range filter

--- scripts/priceRange.js
+++ scripts/priceRange.js
@@ -1,32 +1,31 @@
 const renderPriceRange = () => {
-    //let max = Math.max(...filteredData.map(o => o.Price))
-    console.log(filteredData, data)
-    // $(function () {
-    //     //THIS IS FOR THE RANGE SLIDER.
-    //     //Sorry for yelling...
-    //     let idOfRangeSlider = 'slider-range';
+    $(function () {
+        //THIS IS FOR THE RANGE SLIDER.
+        //Sorry for yelling...
+        let idOfRangeSlider = 'slider-range';
+        let max = Math.max(...filteredData.map(o => o.Price))
+        console.log(max);
+        $('#' + idOfRangeSlider).css('display', 'none');
+        $("<div></div>").insertAfter('#' + idOfRangeSlider);
 
-    //     $('#' + idOfRangeSlider).css('display', 'none');
-    //     $("<div></div>").insertAfter('#' + idOfRangeSlider);
+        var range = $('#' + idOfRangeSlider).next()[0];
 
-    //     var range = $('#' + idOfRangeSlider).next()[0];
-
-    //     noUiSlider.create(range, {
-    //         connect: true,
-    //         start: [5, 68],
-    //         range: {
-    //             'min': 2,
-    //             'max': 200
-    //         },
-    //         step: 10,
-    //         tooltips: [
-    //             wNumb({ decimals: 2, prefix: '$' }),
-    //             wNumb({ decimals: 2, prefix: '$' }),
-    //         ],
-    //     });
-    //     range.noUiSlider.on('update', function (values) {
-    //         $('#' + idOfRangeSlider).val(values.join(' - '));
-    //     });
-    // })();
+        noUiSlider.create(range, {
+            connect: true,
+            start: [5, 68],
+            range: {
+                'min': 2,
+                'max': 200
+            },
+            step: 10,
+            tooltips: [
+                wNumb({ decimals: 2, prefix: '$' }),
+                wNumb({ decimals: 2, prefix: '$' }),
+            ],
+        });
+        range.noUiSlider.on('update', function (values) {
+            $('#' + idOfRangeSlider).val(values.join(' - '));
+        });
+    })();
 }
 renderPriceRange()
