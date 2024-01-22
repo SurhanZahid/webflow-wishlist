c9a07713c56182e4267b8aaac4947134e21ecbf3
SurhanZahid <surhanzahid@gmail.com>
2024-01-22 22:03:11 +0300

    updated the min price range

--- scripts/priceRange.js
+++ scripts/priceRange.js
@@ -3,7 +3,7 @@ const renderPriceRange = () => {
         $(function () {
             let idOfRangeSlider = 'slider-range';
             let max = Math.max(...filteredData.map(o => o.Price))
-            let min = Math.min(...filteredData.map(o => o.Price))
+            let min = 0
             $('#' + idOfRangeSlider).css('display', 'none');
             $("<div></div>").insertAfter('#' + idOfRangeSlider);
 
@@ -22,18 +22,20 @@ const renderPriceRange = () => {
                     wNumb({ decimals: 2, prefix: '$' }),
                 ],
             });
-            range.noUiSlider.on('update', function (values) {
-                // Filter items within the min and max range
-                const filteredArray = defaultState.filter(item => item.Price >= values[0] && item.Price <= values[1]);
+            setTimeout(() => {
+                range.noUiSlider.on('update', function (values) {
+                    // Filter items within the min and max range
+                    const filteredArray = defaultState.filter(item => item.Price >= values[0] && item.Price <= values[1]);
 
-                // Custom comparator function to sort by price
-                const sortByPrice = (a, b) => a.Price - b.Price;
+                    // Custom comparator function to sort by price
+                    const sortByPrice = (a, b) => a.Price - b.Price;
 
-                // Sorting by price after filtering
-                filteredData = filteredArray.slice().sort(sortByPrice);
-                refreshList();
-                $('#' + idOfRangeSlider).val(values.join(' - '));
-            });
+                    // Sorting by price after filtering
+                    filteredData = filteredArray.slice().sort(sortByPrice);
+                    refreshList();
+                    $('#' + idOfRangeSlider).val(values.join(' - '));
+                });
+            }, 1000)
         });
     }, 1000)
 }
