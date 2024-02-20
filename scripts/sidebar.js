6632f1ca6a36be5fa4d20904bc92d23f9db1a118
SurhanZahid <surhanzahid@gmail.com>
2024-02-20 15:35:26 +0300

    updated sidebar script

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,18 +1,21 @@
 // Initial visibility state
 let isCategoriesVisible = true;
 let isBrandVisible = true;
-let isPriceRangeVisiable = true;
-
+let isPriceRangeVisible = true;
+let isRelevanceVisible = true;
 // Elements selection
 const categoryList = document.getElementById('categories-list');
 const brandList = document.getElementById('brand-list');
-const priceRangeContainer = document.getElementById('price-range-container')
+const priceRangeContainer = document.getElementById('price-range-container');
+const relevanceContainer = document.getElementById('relevance-container')
 const categoryDropdown = document.getElementById('category-header');
 const brandDropdown = document.getElementById('brand-header');
-const priceRangeDropddown = document.getElementById('price-header');
+const priceRangeDropdown = document.getElementById('price-header');
+const relevanceDropdown = document.getElementById('relevance-header')
 const categoryIcon = document.getElementById('category-side-icon');
 const brandIcon = document.getElementById('brand-side-icon');
 const priceIcon = document.getElementById('price-side-icon');
+const releveanceIcon = document.getElementById('relevance-side-icon');
 
 const toggleSideIcon = (element, isVisible) => {
     element.src = isVisible ? 'https://assets-global.website-files.com/65297b8483de877bf51c6220/65c3ebe8b6eb5f7598a2f79e_chevron-down.svg' : 'https://uploads-ssl.webflow.com/65297b8483de877bf51c6220/65c400003f45e9c93811ac7f_chevron-up.svg'
@@ -27,9 +30,9 @@ const toggleVisibility = (element, isVisible) => {
 // Hide dropdowns by default
 categoryList.style.display = 'none';
 brandList.style.display = 'none';
-priceRangeContainer.style.display = 'none'
-
-// Event listeners for category dropdown
+priceRangeContainer.style.display = 'none';
+relevanceContainer.style.display = 'none';
+// Event listeners for category dropdowns
 categoryDropdown.addEventListener('click', () => {
     isCategoriesVisible = !isCategoriesVisible;
     toggleVisibility(categoryList, isCategoriesVisible);
@@ -42,8 +45,14 @@ brandDropdown.addEventListener('click', () => {
     toggleVisibility(brandList, isBrandVisible);
     toggleSideIcon(brandIcon, isBrandVisible);
 });
-priceRangeDropddown.addEventListener('click', () => {
-    isPriceRangeVisiable = !isPriceRangeVisiable;
-    toggleVisibility(priceRangeContainer, isPriceRangeVisiable);
-    toggleSideIcon(priceIcon, isPriceRangeVisiable);
+priceRangeDropdown.addEventListener('click', () => {
+    isPriceRangeVisible = !isPriceRangeVisible;
+    toggleVisibility(priceRangeContainer, isPriceRangeVisible);
+    toggleSideIcon(priceIcon, isPriceRangeVisible);
+})
+
+relevanceDropdown.addEventListener('click', () => {
+    isPriceRangeVisible = !isPriceRangeVisible;
+    toggleVisibility(relevanceContainer, isRelevanceVisible);
+    toggleSideIcon(releveanceIcon, isRelevanceVisible);
 })
