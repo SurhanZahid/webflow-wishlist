e1f711b43cb40702859b9d9fdfc89434f62ac7e6
SurhanZahid <surhanzahid@gmail.com>
2025-03-13 15:19:14 +0500

    updated packages classes name

--- scripts/Wishlist/addBoxesToWishlist.js
+++ scripts/Wishlist/addBoxesToWishlist.js
@@ -98,7 +98,7 @@ const refreshButtonStyles = () => {
 /*************************************
  * Set Up Click Handlers (Renamed)
  *************************************/
-const altProductLists = document.querySelectorAll('.product-list .pd0');
+const altProductLists = document.querySelectorAll('.product-list .pd0, .packages-block');
 
 if (altProductLists.length > 0) {
     altProductLists.forEach((altItem) => {
