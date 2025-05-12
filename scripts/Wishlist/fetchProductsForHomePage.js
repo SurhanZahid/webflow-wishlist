095dc01d0c673e9c56a263804b9201e5186bb6bd
SurhanZahid <surhanzahid@gmail.com>
2025-05-13 01:14:44 +0500

    hot fix

--- scripts/Wishlist/fetchProductsForHomePage.js
+++ scripts/Wishlist/fetchProductsForHomePage.js
@@ -18,10 +18,13 @@ const fetchAndRenderProducts = async () => {
 
     console.log(collection_name);
 
-    const categorySlug = !collection_name ? 'home' : '';
-    const collectionSlug = collection_name;
+    const isValidCollection = typeof collection_name === 'string' && collection_name.trim() !== '';
+
+    const categorySlug = isValidCollection ? '' : 'home';
+    const collectionSlug = isValidCollection ? collection_name : '';
 
     const endpoint = `https://merchos.gemnote.com/api/v1/products/?is_active=&has_variants=&can_be_customized=&min_price=&max_price=&brand_slug=&category_slug=${categorySlug}&collection_slug=${collectionSlug}`;
+
 
 
     try {
